f924108f739b0df707f502c38a067987dde1ee5c
kara4k <sif4bag2>
2020-09-29 17:49:59 +0300

    TEAM-1 broken commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
+        System.out.println("TeamCity")
         System.out.println("TeamCity");
         System.out.println("TeamCity");
     }
