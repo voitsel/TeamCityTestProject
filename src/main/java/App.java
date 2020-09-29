3b19c7f2e22458162a457958391e092615aefe19
kara4k <sif4bag2>
2020-09-29 17:07:22 +0300

    TEAM-1 first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
+        System.out.println("TeamCity");
     }
 }
