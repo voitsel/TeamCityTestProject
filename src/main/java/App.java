ae3a41fa71be664ac64d84ec7162126e8d5c1a8e
kara4k <sif4bag2>
2020-09-29 17:05:09 +0300

    TEAM-1 initial

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,5 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
-        System.out.println("TeamCity");
     }
 }
