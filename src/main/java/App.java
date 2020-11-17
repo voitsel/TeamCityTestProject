47f770f438709c889d3862c58a5cbc78b8dac529
a.voitsel <a.voitsel@gpsolutions.com>
2020-11-17 21:19:02 +0300

    TEAM-1 second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
+        System.out.println("TeamCity");
     }
 }
