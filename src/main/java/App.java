50f3954caf386ade7e013e47be33ab5ed1c4b9b9
kara4k <sif4bag2>
2020-11-17 17:22:26 +0300

    TEAM-1 new commit to git

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity")
+        System.out.println("TeamCity");
+        System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
     }
