3f519e119853737582eed6311746916c2f53c791
Bob <bob@bobson.com>
2020-11-18 18:13:32 +0300

    TEAM-1 fixed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity")
-        System.out.println("TeamCity")
+        System.out.println("TeamCity");
+        System.out.println("TeamCity");
     }
 }
