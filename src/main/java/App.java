582f0e99dc7dd899238e8c96eb1cee2cd6e22e1b
Bob <bob@bobson.com>
2020-11-19 14:09:25 +0300

    TEAM-1 break partly

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,5 +13,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity")
+        System.out.println("TeamCity")
     }
 }
