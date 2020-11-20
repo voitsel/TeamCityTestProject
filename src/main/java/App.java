bd81bcd9d9ba8141daeb3eaec9d9739b2b2be886
charlie <charlie@charleson.com>
2020-11-20 17:29:33 +0300

    TEAM-1 break build 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,7 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity")
+        System.out.println("TeamCity");
         System.out.println("TeamCity")
     }
 }
