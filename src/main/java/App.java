8f950434bc99c2a9cb45f299444546f1f4fb433b
Bob <bob@bobson.com>
2020-12-07 13:00:54 +0300

    TEAM-1 break build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity")
+        System.out.println("TeamCity")
     }
 }
