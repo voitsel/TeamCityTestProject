392ac6a7617e56694e3cf0f2b381134a473940e1
alice <alice@gmail.com>
2020-12-09 13:21:37 +0300

    new commit from alice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
+        System.out.println("Team");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
