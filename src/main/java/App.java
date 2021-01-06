1c237b349a2ff92f7acc79501bc87d93f7f97ebb
voitsel <70640061+voitsel@users.noreply.github.com>
2021-01-06 17:59:14 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
+        
     }
 }
