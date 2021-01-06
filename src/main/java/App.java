4c68bcac3fd6b40aa8a229fa5e3294dad5b28cdd
kara4k <sif4bag2>
2021-01-06 17:41:47 +0300

    JENKINS-1 fix buikd

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity")
-        System.out.println("TeamCity")
+        System.out.println("TeamCity");
+        System.out.println("TeamCity");
     }
 }
