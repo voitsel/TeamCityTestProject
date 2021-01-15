828d718478659fed26624f3711378b681df8ec54
Bob <bob@bobson.com>
2021-01-15 19:31:17 +0300

    MASTER ignore test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,17 @@
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AppTest {
 
 	@Test
 	public void successResultTest() {
+		Assert.assertTrue(App.getSuccessResult());
+	}
+	
+	@Test
+	@Ignore
+	public void ignoredTest() {
 		Assert.assertTrue(App.getSuccessResult());
 	}
 
