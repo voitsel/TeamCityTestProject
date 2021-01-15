1a712c554e6281a671af1f9b3d42753a045b38af
Bob <bob@bobson.com>
2021-01-15 19:21:53 +0300

    MASTER remove souts

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,9 +10,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
-        System.out.println("TeamCity");
-        System.out.println("TeamCity");
 
     }
 
