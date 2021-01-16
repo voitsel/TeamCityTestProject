5ecfe3f6f71293165b482b6bbd3ff49e786ea155
Bob <bob@bobson.com>
2021-01-16 09:42:54 +0300

    MASTER add tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,8 +10,25 @@ public class AppTest {
 	}
 
 	@Test
+	public void successResultTest2() {
+		Assert.assertTrue(App.getSuccessResult());
+	}
+
+	@Test
+	public void successResultTest3() {
+		Assert.assertTrue(App.getSuccessResult());
+	}
+
+
+	@Test
 	@Ignore
 	public void ignoredTest() {
+		Assert.assertTrue(App.getSuccessResult());
+	}
+
+	@Test
+	@Ignore
+	public void ignoredTest2() {
 		Assert.assertTrue(App.getSuccessResult());
 	}
 
