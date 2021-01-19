b2fe8cf3d6420ab4f79b5660ccef99194a8cc89f
Aliaksei Voitsel <voitsel@gmail.com>
2021-01-19 11:48:55 +0300

    MASTER unignore test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,8 +10,6 @@ public class App {
         System.out.println("TeamCity");
         System.out.println("TeamCity");
         System.out.println("TeamCity");
-        System.out.println("TeamCity");
-        System.out.println("TeamCity");
 
     }
 
