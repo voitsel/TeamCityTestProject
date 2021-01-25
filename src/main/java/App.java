cdb64a75b0c99ee57f79d59d7035a6977fa6a60e
aleksandragerasimova <aleksandragerasimova1804@gmail.com>
2021-01-25 15:35:14 +0300

    new commit from alice

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("TeamCity");
-        System.out.println("Teamazsxdcfvgbhnjhbgvfcdx");
+        System.out.println("Teamazsxdcfvgbhорпавnjhbgvfcdx");
         System.out.println("TeamCity");
         System.out.println("TeamCity11111");
         System.out.println("TeamCity");
